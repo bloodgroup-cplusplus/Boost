01d4ee3eed0b20d227e6ad0ba7dc0a804963d993
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-07-24 19:16:07 +0530

    Update any.cpp

--- any.cpp
+++ any.cpp
@@ -6,12 +6,12 @@ int main()
 {
 	std::vector<boost::any> some_values;
 	some_values.push_back(10);
-	const char * c_str="Hello there";
+	const char * c_str="Hello there";;
 	some_values.push_back(c_str);
 //	for(auto x: some_values)
 //	{
 //		std::cout<<x<<std::endl;
 //	}
-	return 0;
+	return 0;;
 }
 
